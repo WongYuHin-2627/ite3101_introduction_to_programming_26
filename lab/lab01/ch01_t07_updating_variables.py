dc25e55207dbc663af003360250c9e5cdc1f5de8
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-03 09:07:02 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -17,6 +17,6 @@ november_rainfall = 5.06
 annual_rainfall += august_rainfall
 
 december_rainfall = 4.06
-annual_rainfall += august_rainfall
+annual_rainfall += december_rainfall
 
 annual_rainfall += 
