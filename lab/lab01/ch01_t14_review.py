ef11980728648fee9383abf491f1c3074d76e6b2
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-05 14:34:51 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,4 +1,4 @@
 exercises_completed = 13
 points_per_exercise = 5
 point_total = 100
-point_total = str(exercises_completed) + str(points_per_exercise)
+point_total += str(exercises_completed) + str(points_per_exercise)
