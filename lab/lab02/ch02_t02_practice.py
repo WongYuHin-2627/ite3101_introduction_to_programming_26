bf22af6d75b95729bec922f575a53f289f68dc09
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-05 15:01:43 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
 # Assign your variables below, each on its own line!
-set caeser to
-set praline
-set viking 
+set caeser to "G"
+set praline to ""
+set viking to ""
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
