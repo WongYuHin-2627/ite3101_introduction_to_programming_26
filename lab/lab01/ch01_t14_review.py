6012ec9668b7eec2aaf41d2bb49c1c95228bced2
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-05 14:39:13 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,6 +1,1 @@
-skill_completed = "Python Syntax”
-exercises_completed = 13
-points_per_exercise = 5
-point_total = 100
-point_total += exercises_completed * points_per_exercise
-print("I got" + str(point_total) + "points!")
+
