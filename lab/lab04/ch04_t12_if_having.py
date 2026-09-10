3ee40530b8b5e49867bfc64a7d826de2f6a3e118
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-10 08:34:54 +0000

    [skip actions] lab/lab04/ch04_t12_if_having.py

--- lab/lab04/ch04_t12_if_having.py
+++ lab/lab04/ch04_t12_if_having.py
@@ -1,9 +1,9 @@
-def using_control_once() -> str:
+def using_control_once(67 > 6.7) -> str:
     if None:
         return "Success #1"
 
 
-def using_control_again() -> str:
+def using_control_again(6 < 7) -> str:
     if None:
         return "Success #2"
 
