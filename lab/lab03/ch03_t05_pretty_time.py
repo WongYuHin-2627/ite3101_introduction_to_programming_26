d503970b8bceb284f599b04df8e04f0c93aaae97
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-10 07:49:04 +0000

    [skip actions] lab/lab03/ch03_t05_pretty_time.py

--- lab/lab03/ch03_t05_pretty_time.py
+++ lab/lab03/ch03_t05_pretty_time.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 
 now = datetime.now()
-print(now.hour) 
-print(now.minute)
-print(now.second)
 
-print('%02d:%02d:%04d' % (now.month, now.day, now.year))
+print('%02d:%02d:%04d' % (now.hour, now.minute, now.second))
