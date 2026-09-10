589854db02c0e8c3848bd64b2004158a8ad51663
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-10 08:41:36 +0000

    lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -6,7 +6,7 @@ def grade_converter(grade: int) -> str:
         return "B"
     elif grade>= 70:
         return "C"
-    elif grade:
+    elif grade >= 65 and grade :
         return "D"
     else:
         return "F"
