f015c61c1be0b94d402256ab8a9058b212ac8335
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-10 08:24:19 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -10,7 +10,7 @@ bool_two = (6 < 67) and (6 < 7)
 bool_three = (67 < 66) or (6 > 7)
 
 # Make me true!
-bool_four = None
+bool_four = (41<67) and "Sigma" == "Diddy"
 
 # Make me true!
 bool_five = None
