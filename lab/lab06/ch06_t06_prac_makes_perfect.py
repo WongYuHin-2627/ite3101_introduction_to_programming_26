3f19c3811ffd51a4bec95a6f83e07e1d66f2d4bf
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-17 08:07:37 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -2,8 +2,8 @@ def cube(n: int):
     return n*n*n
 
 
-def by_three(n:int):
-    if cube % 3 ==0:
-        return "cube%3"
+def by_three(n: int):
+    if cube % 3 == 0:
+        return cube(n)
     else:
         return False
