c28b2af35a4708f43bf795ac079442f6ab9d76e8
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-17 07:17:59 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -1,10 +1,9 @@
 pyg = 'ay'
 
 original = input('Enter a word:')
-
+word = original.lower()
+first = word[0]
 if len(original) > 0 and original.isalpha():
     print(original)
 else:
     print('empty')
-word = original.lower()
-first = word[0]
