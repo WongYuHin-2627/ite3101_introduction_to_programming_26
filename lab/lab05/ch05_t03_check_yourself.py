2def89c3fb42137dac850d0652e902670b37fb80
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-17 07:10:09 +0000

    [skip actions] lab/lab05/ch05_t03_check_yourself.py

--- lab/lab05/ch05_t03_check_yourself.py
+++ lab/lab05/ch05_t03_check_yourself.py
@@ -3,6 +3,6 @@ print('Welcome to the Pig Latin Translator!')
 # Start coding here!
 original = input("Enter a word:")
 if len(original) > 0
+
     else
-    
-    print "empty"
+        print "empty"
