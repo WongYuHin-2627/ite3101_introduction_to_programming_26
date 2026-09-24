d7c7dd5b177807a4661126f8616df8ee7cd09646
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-24 08:39:59 +0000

    [skip actions] lab/lab07/ch07_t06_never_know.py

--- lab/lab07/ch07_t06_never_know.py
+++ lab/lab07/ch07_t06_never_know.py
@@ -23,6 +23,5 @@ def rental_car_cost(days: int) -> int:
 
 
 def trip_cost(city: str, days, spending_money: int = 0: int) -> int:
-    return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
+    return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money: int = 0
 
-def  spending_money()
