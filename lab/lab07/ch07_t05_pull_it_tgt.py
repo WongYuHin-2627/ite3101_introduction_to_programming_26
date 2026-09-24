718a653aa1ec295c462c0ac6e22bbff5ad331ac6
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-24 08:36:50 +0000

    [skip actions] lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -2,7 +2,7 @@ def hotel_cost(nights: int) -> int:
     return 140 * nights
 
 
-def plane_ride_cost(city: int) -> int:
+def plane_ride_cost(city: str) -> int:
     if city == "Charlotte":
         return 183
     elif city == "Tampa":
