b8b6433870749ed9e4f9a052d61d912ee84d3f24
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-24 08:08:24 +0000

    [skip actions] lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -1,11 +1,11 @@
 def hotel_cost(nights: int) -> int:
     return 140 * nights
 def  plane_ride_cost(city):
-    if city == "Charlotte" 
+    if city == "Charlotte":
         return 183
-Tampa
+if city == "Tampa":
         return 220
-"Pittsburgh": 
+if city =="Pittsburgh": 
 return 222
         if city =="Los Angeles": 
         return 475
