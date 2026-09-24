82d1e954c210132e0505f3f15e4c5e6608b7828f
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-24 08:37:01 +0000

    [skip actions] lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -22,5 +22,5 @@ def rental_car_cost(days: int) -> int:
     return cost
 
 
-def trip_cost(days, city):
+def trip_cost(days,city):
     return rental_car_cost(days) + hotel_cost(days-1) + plane_ride_cost(city)
