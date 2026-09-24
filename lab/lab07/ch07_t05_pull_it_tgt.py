bb53238296beba7176376e81aa1140f1b9e2a075
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-24 08:34:34 +0000

    [skip actions] lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -15,7 +15,7 @@ def plane_ride_cost(city: str) -> int:
 
 def rental_car_cost(days: int) -> int:
     cost = 40 * days
-    if days >= 7:
+    if days = 7 or days > 7:
         cost -= 50
     elif days >= 3:
         cost -= 20
