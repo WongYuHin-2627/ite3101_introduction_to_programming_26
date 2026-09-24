3d1b46adb1bbecb317d34f9185ef4e3f3af148eb
WongYuHin-2627 <wilsonwong2214@gmail.com>
2026-09-24 07:37:58 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,7 +1,8 @@
-def shut_down(message:str):
+def shut_down(message: str) -> str:
+    return message
     if shut_down == "yes":
         return "Shutting down"
     elif shut_down == "no":
-        return "Shutdown aborted" 
+        return "Shutdown aborted"
     else:
         return "sorry"
